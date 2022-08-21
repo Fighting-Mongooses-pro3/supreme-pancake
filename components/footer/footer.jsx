0934b95f3ce9c245e06f864cf2e23fe065055440
Andrew87E <106359255+Andrew87E@users.noreply.github.com>
2022-08-21 13:54:22 -0500

    folder structure work

---
+++ components/footer/footer.jsx
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+    
+    
+    return(
+        <div>
+            Footer goes here
+        </div>
+    )
+}
---
+++ components/templates/page/page.jsx
@@ -0,0 +1,2 @@
+// navbar and footer template so therye static for every page
+
