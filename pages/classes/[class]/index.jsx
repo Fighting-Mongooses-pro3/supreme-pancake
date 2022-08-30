b9a4a1de9f4eb451368a44182fdfd98356386d6f
Brometheus <zs.serrano1@gmail.com>
2022-08-30 01:23:37 -0500

    added section info to class index.jsx

--- pages/classes/[class]/index.jsx
+++ pages/classes/[class]/index.jsx
@@ -28,6 +28,28 @@ const DndClass = () => {
       <Aside 
       side=""
       />
+      <section>
+        Hit Die: 1d{dndClass.hit_die}.  Your character has a pool of hit dice in an amount equal to their level that they can utilize to heal during a short rest.
+
+        Your class allows you to select beginning skill proficiencies from a given set of options. {dndClass.proficiency_choices.desc}
+        Along with skills, your character class comes with proficiencies in weapons, armor, saving throws, and possibly some extras.
+        The proficiencies for {dndClass.index} are listed below:
+        {dndClass.proficiencies}
+
+        Each class comes with a set of standard starting equipment, including sections where you're asked to select from a couple options. Below you'll find the starting equipment list for the {dndClass.index}. We recommend speaking with your DM to see if there are any additions or exclusions to go with this list for your starting character.
+        {dndClass.starting_equipment}
+        {dndClass.starting_equipment_options}
+
+        As you level up your {dndClass.index}, your character may be granted new abilities and features.  Below you'll find a guide for understanding what your {dndClass.index} is given at each level.
+        {dndClass.class_levels}
+
+        If your character is starting over level 1, there is a chance that you might want to multi-class into {dndClass.index}.
+        Below you'll find a guide to what prerequisite your character needs to have in order to be able to multi-class into {dndClass.index}, as well as a list of proficiencies that are added to your character if they do not already have them. We recommend consulting with your DM before multi-classing.
+        {dndClass.multi_classing}
+
+        Depending on your starting level and class, your character might be given the option of taking a subclass already.  Below you'll find an example of a subclass for the {dndClass.index}.
+        {dndClass.subclasses}
+      </section>
       </Container>
     </Page>
   );
