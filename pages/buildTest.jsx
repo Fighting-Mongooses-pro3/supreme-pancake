b6f2d53342b835dc724300de36a40f3257a29879
Jaden <jadentr.44@gmail.com>
2022-09-01 16:59:13 -0500

    page can add and render cards, still some bugs

---
+++ pages/buildTest.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react'
+import Draggable from 'react-draggable';
+import ReactDOM from 'react-dom';
+
+export default function buildTest() {
+   let gridX;
+   let cardX;
+   let card1;
+  if(typeof window !== "undefined"){
+    gridX = (document.getElementById("page").clientWidth)
+    cardX = Math.round(gridX*.40)
+    card1 = document.getElementById("card1")
+  }
+  const [componentEl, setComponents] = useState();
+  // const [cardInfo, setCardInfo] = useState([]);
+  let cardInfo = []
+  function addCard(){
+    cardInfo.push({text:"new card"})
+    console.log(cardInfo)
+  }
+
+  function render(){
+    
+    setComponents(cardInfo.map(e=>{return <Draggable grid={[gridX/2, 20]} scale={1}><div id='card1'  className='relative left-5 top-5 w-2/5 h-1/4 border'>{e.text}</div></Draggable> }))
+    
+  }
+
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <div>
+      <button className='border' onClick={()=>render()}>render</button> <br />
+      <button className='border' onClick={()=>addCard()}>add</button>
+      </div>
+    <div id='page' className=' w-1/4 h-2/3  overflow-hidden   border '>
+
+    {componentEl}
+    
+    {/* <Draggable grid={[gridX/2, 20]} scale={1}><div id='card1'  className='relative left-5 top-5 w-2/5 h-1/4 border'>card</div></Draggable>  */}
+  
+
+
+    </div>
+    </div>
+    
+    
+  )
+}
