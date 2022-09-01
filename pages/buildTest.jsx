5675ef6459dc35f566ee59dd2c21a18447102084
Jaden <jadentr.44@gmail.com>
2022-09-01 17:03:57 -0500

    not sure what i did but state seems to work now...

--- pages/buildTest.jsx
+++ pages/buildTest.jsx
@@ -12,23 +12,25 @@ export default function buildTest() {
     card1 = document.getElementById("card1")
   }
   const [componentEl, setComponents] = useState();
-  // const [cardInfo, setCardInfo] = useState([]);
-  let cardInfo = []
+  const [cardInfo, setCardInfo] = useState([]);
+  // let cardInfo = []
+
   function addCard(){
     cardInfo.push({text:"new card"})
-    console.log(cardInfo)
+    renderCard()
   }
 
-  function render(){
+  function renderCard(){
     
-    setComponents(cardInfo.map(e=>{return <Draggable grid={[gridX/2, 20]} scale={1}><div id='card1'  className='relative left-5 top-5 w-2/5 h-1/4 border'>{e.text}</div></Draggable> }))
+    let cardEls = cardInfo.map(e=>{return <Draggable grid={[gridX/2, 20]} scale={1}><div id='card1'  className='relative left-5 top-5 w-2/5 h-1/4 border'>{e.text}</div></Draggable> })
+    setComponents(cardEls)
     
   }
 
   return (
     <div className='flex justify-center items-center h-screen'>
       <div>
-      <button className='border' onClick={()=>render()}>render</button> <br />
+      <button className='border' onClick={()=>renderCard()}>render</button> <br />
       <button className='border' onClick={()=>addCard()}>add</button>
       </div>
     <div id='page' className=' w-1/4 h-2/3  overflow-hidden   border '>
