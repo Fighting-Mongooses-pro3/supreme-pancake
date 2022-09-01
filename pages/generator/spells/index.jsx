38d7d1c30fd2d76786277060ac788ddb223b4a0f
Andrew87E <106359255+Andrew87E@users.noreply.github.com>
2022-08-31 23:06:11 -0500

    moved some stuff around

--- pages/generator/spells/index.jsx
+++ pages/generator/spells/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Card, Container, Page } from "../../components";
+import { Card, Container, Page } from "../../../components";
 import axios from "axios";
 
 const Spells = () => {
