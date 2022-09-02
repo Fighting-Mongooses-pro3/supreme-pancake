0162bbce280a6d966b95e284ae16c725e5aa1977
Jeffrey Littleton <littletonjeffrey@gmail.com>
2022-09-02 16:37:47 -0500

    update sidebar

--- components/sidebar/sidebar.jsx
+++ components/sidebar/sidebar.jsx
@@ -1,15 +1,40 @@
 import React from 'react'
-
+<FontAwesomeIcon icon="fa-solid fa-pen-to-square" />
 export const Sidebar = () => {
   return (
     <sidebar>
         <ul className="toolbar">
-          <li><button id='text__generator'>Text</button></li>
-          <li><button id='npc__generator'>NPC</button></li>
-          <li><button id='monster__generator'>Monster</button></li>
-          <li><button id='spell__generator'>Spell</button></li>
-          <li><button id='import__image'>Image</button></li>
-          <li><button id='past_components'>Prev. Component</button></li>
+          <li>
+            <button 
+              id='add__text'
+              text='Text'
+              FontAwesomeIcon icon="fa-solid fa-pen-to-square"
+              /></li>
+          <li><button 
+              id='add__NPC'
+              text='NPC'
+              FontAwesomeIcon icon="fa-solid fa-users"
+              /></li>
+          <li><button 
+              id='add__monster'
+              text='Monster'
+              FontAwesomeIcon icon="fa-solid fa-dragon" 
+              /></li>
+          <li><button 
+              id='add--spell'
+              text='Spell'
+              FontAwesomeIcon icon="fa-solid fa-scroll" 
+              /></li>
+          <li><button 
+              id='import__image'
+              text='Image'
+              FontAwesomeIcon icon="fa-regular fa-image"
+              /></li>
+          <li><button 
+              id='my__components'
+              text='My Components'
+              FontAwesomeIcon icon="fa-regular fa-folder-open"
+              /></li>
       </ul>
      
     </sidebar>
