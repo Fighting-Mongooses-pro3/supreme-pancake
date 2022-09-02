a075ae50d66a3767e0588ab22fbf540205fd47ab
Jeffrey Littleton <littletonjeffrey@gmail.com>
2022-09-01 22:08:15 -0500

    sidebar compnent

--- components/sidebar/sidebar.jsx
+++ components/sidebar/sidebar.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 export const Sidebar = () => {
   return (
-    <div>
-        
-    </div>
+    <sidebar>
+        <ul className="toolbar">
+          <li><button id='text__generator'>Text</button></li>
+          <li><button id='npc__generator'>NPC</button></li>
+          <li><button id='monster__generator'>Monster</button></li>
+          <li><button id='spell__generator'>Spell</button></li>
+          <li><button id='import__image'>Image</button></li>
+          <li><button id='past_components'>Prev. Component</button></li>
+      </ul>
+     
+    </sidebar>
   )
 }
