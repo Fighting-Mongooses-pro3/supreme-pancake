b5b54457a6ebe5176b586fed8452e0f0226702fd
Andrew87E <106359255+Andrew87E@users.noreply.github.com>
2022-09-03 18:26:33 -0500

    scroll effect started

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,24 +1,61 @@
 import Head from "next/head";
+import $ from "jquery";
 import Image from "next/image";
 import { Page, Container, BuildTools, DisplayBuild } from "../components";
+import { useState, useEffect } from "react";
 
 export default function Home() {
-  console.log('TEST')
-  
-  return (
-    <>
-      <Page currentPage="Home">
-        <Container customCss="flex justify-center bg-transparent drop-shadow-md min-h-min mr-1 overflow-hidden min-w-min h-screen">
-          <BuildTools />
-          <DisplayBuild />
-          {/* <section id="tools" className="bg-green-600 text-purple-400">
-            TOOLS GO HERE
-          </section>
-          <section id="build" className="bg-cyan-400 text-red-600">
-            Welcome to the Jungle
-          </section> */}
-        </Container>
-      </Page>
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(false);
+  }, []);
+
+  {
+    if (loading) {
+      return <div>Loading...</div>;
+    } else {
+      if (typeof window !== "undefined") {
+        const topEl = $("#top");
+        const bottomEl = $("#bottom");
+        console.log($(window))
+        $(window).on("scroll", () => {
+          topEl.css({
+            width: 100 - scroll / 5 + "%",
+            top: -(scroll / 10) + "%",
+            "-webkit-filter": "blur(" + scroll / 100 + "px)",
+            filter: "blur(" + scroll / 100 + "px)",
+          });
+          console.log(topEl)
+          console.log('THIS', this)
+        });
+
+        return (
+          <>
+            <Page currentPage="Home">
+              <Container customCss="flex justify-center bg-transparent drop-shadow-md min-h-min mr-1 overflow-hidden min-w-min h-screen">
+                <section id="contain">
+                  <section id="top" className="bg-red-600 bg-opacity-95 z-10">
+                    <Image
+                      src="/landing__images/dreams-g0534bb22a_1280.jpg"
+                      alt="Fantasy book in the sky with waterfalls coming out of it"
+                      layout="fill"
+                    />
+                  </section>
+
+                  <section id="bottom" className="bg-opacity-95 bg-lime-500">
+                    <Image
+                      src="/landing__images/fantasy-g896efbe29_1920.jpg"
+                      alt="Fantasy book in the sky with waterfalls coming out of it"
+                      layout="fill"
+                    />
+                  </section>
+                </section>
+              </Container>
+            </Page>
+          </>
+        );
+      }
+    }
+  }
 }
