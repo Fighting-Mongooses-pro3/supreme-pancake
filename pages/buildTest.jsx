a5c1a253c3f8f5e9a6acd0d44f40642a50e54dc1
Jaden <jadentr.44@gmail.com>
2022-09-02 21:31:13 -0500

    it is saving the y based off %

--- pages/buildTest.jsx
+++ pages/buildTest.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef } from "react";
 import Draggable from "react-draggable";
-import ReactToPrint from "react-to-print";
+import { useReactToPrint } from 'react-to-print';
 
 export default function buildTest() {
   const [componentEl, setComponents] = useState("");
@@ -8,18 +8,44 @@ export default function buildTest() {
   
   
   function addCard(loc){
-    cardInfo.push({ text: "new card", location:loc });
+    cardInfo.push({id:`card${cardInfo.length}`, text: "new card", location:loc,y:null });
 
-    let cardEls = cardInfo.map((e)=>{
+    let cardEls = cardInfo.map((e,i)=>{
       let side = e.location ==="l"?"left-[5%]":"right-[5%]";
-     return <Draggable axis="y" grid={[20, 20]} scale={1}>
-          <div id="card1" className={`bg-red-300 absolute text-3xl ${side} w-2/5 h-1/4 border`}>
+     return <Draggable   onStop={handleStop}  axis="y" grid={[20, 20]} scale={1}>
+          <div id={`card${i}`} className={`bg-red-300 absolute text-3xl ${side} w-2/5 h-1/4 border`}>
             {e.text}
           </div>
         </Draggable>
     });
     console.log(cardInfo)
     setComponents(cardEls);
+  }
+
+  const handleStop = (event, dragElement) => {
+    let currentEl = document.getElementById(dragElement.node.id)
+    cardInfo.forEach((e,i)=>{
+      if(e.id == dragElement.node.id){
+        console.log("page y :", document.getElementById('page').clientHeight)
+        console.log("distance y :",dragElement.y)
+        e.y = (dragElement.y / document.getElementById('page').clientHeight * 100) + '%'
+        console.log(e)
+      }
+    })
+  };
+  
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current,
+  });
+  async function print(){
+    await cardInfo.forEach((e,i)=>{
+      document.getElementById(e.id).style.transform = "translate(0,0)"
+    })
+    await cardInfo.forEach((e,i)=>{
+      document.getElementById(e.id).style.top = e.y
+    })
+    
+    handlePrint()
   }
 
   const componentRef = useRef();
@@ -33,14 +59,11 @@ export default function buildTest() {
         </button>
         <br />
         <button className="border" onClick={() => addCard("l")}>add left </button>
-        <ReactToPrint
-        trigger={() => <button className="border">Print this out!</button>}
-        content={() => componentRef.current}
-      />
+        <button onClick={()=> print()}>print</button>
       </div>
     <div
       id="page"
-      className=" w-[65vh] h-[85vh]   overflow-auto    relative border "
+      className=" w-[65vh] h-[85vh]   overflow-hidden    relative border "
        
     >
       <div ref={componentRef} className='h-auto w-auto' >
