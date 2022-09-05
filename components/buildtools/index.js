9fe79c81c385a8b153b5ff20c6a166e61f55228c
Andrew87E <106359255+Andrew87E@users.noreply.github.com>
2022-09-05 17:20:35 -0500

    cleanup

---
+++ components/buildtools/index.js
@@ -0,0 +1,14 @@
+export { BuilderCheckbox } from "./BuilderComponents/BuilderCheckbox/BuilderCheckbox";
+export { BuilderDerivedValueInput } from "./BuilderComponents/BuilderDerivedValueInput/BuilderDerivedValueInput";
+export { BuilderInput } from "./BuilderComponents/BuilderInput/BuilderInput";
+export { useBuilderContext } from "./BuilderContext/BuilderContext";
+export { BuilderStatblock } from "./BuilderStatblock/BuilderStatblock";
+export { BuilderEntityHeader } from "./BuilderStatblock/BuilderEntityHeader/BuilderEntityHeader";
+export { BuilderEntityAttributes } from "./BuilderStatblock/BuilderEntityAttributes/BuilderEntityAttributes";
+export { BuilderEntityAbilityScoreRow } from "./BuilderStatblock/BuilderEntityAbilityScoreRow/BuilderEntityAbilityScoreRow";
+export { BuilderEntityAbilities } from "./BuilderStatblock/BuilderEntityAbilities/BuilderEntityAbilities";
+export { BuilderEntitySkills } from "./BuilderStatblock/BuilderEntitySkills/BuilderEntitySkills";
+export { EntityBuilder } from "./EntityBuilder/EntityBuilder";
+export { MonsterBuilder } from "./MonsterBuilder/MonsterBuilder";
+export { MonsterForm } from "./MonsterBuilder/MonsterForm/MonsterForm";
+
--- pages/index.jsx
+++ pages/index.jsx
@@ -18,7 +18,7 @@ export default function Home() {
           <>
             <Page currentPage="Home">
             <Parallax pages={4} >
-
+              
             </Parallax>
             </Page>
           </>
