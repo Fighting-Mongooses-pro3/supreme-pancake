2e49eb2a1d62e35d4a3a32136e9e90b0e73378a6
Brometheus <zs.serrano1@gmail.com>
2022-09-07 21:14:29 -0500

    final styling for monsterxp and characterxp files

--- components/encounters/CharacterXp.jsx
+++ components/encounters/CharacterXp.jsx
@@ -41,101 +41,109 @@ export const CharacterXp = () => {
 
   return (
     <div>
-      <p className="text-center ">
-        Below you will find two buttons that can add or remove rows to this
-        piece of the Encounter Balancer. Each row will have two input fields:
-        one for the level of the characters in your party, and another for how
-        many characters are at that level currently. You can then add more rows
-        for each unique level of characters that comprise the party.
-        <br></br>
-        <br></br>
-        For example, we will take a party of five characters, three of which are
-        level 8, and two of which are level 9. There are two different levels
-        represented here so you would need two rows. One row would have the
-        character level field set to 8, and the number of characters field set
-        to 3. The second row would have the character level field set to 9, and
-        the number of characters field set to 2. Once these fields have been
-        input, you will see that the difficulty level fields below will
-        auto-populate with the total XP thresholds for the entirety of the
-        party.
-      </p>
-      <br></br>
-      <br></br>
-      <div className=" ">
-        <button
-          className="border rounded-3xl border-black p-2 m-2 hover:bg-white bg-green-300"
-          onClick={() => {
-            // Add an empty object to the thresholds array
-            setXpThresholds([...xpThresholds, { level: 0, numChars: 0 }]);
-          }}
-        >
-          Add New Character Level
-        </button>
-        <button
-          className="border rounded-3xl border-black p-2 hover:bg-white bg-red-400"
-          onClick={() => {
-            // Remove an object from the thresholds array
-            setXpThresholds([...xpThresholds.slice(0, -1)]);
-          }}
-        >
-          Remove Character Level
-        </button>
-        <br></br>
-        <div>
-          {xpThresholds.map((threshold, i) => (
-            <div key={"threshold" + i} className="flex">
-              <div>
-                <label htmlFor={"character-level-input-" + i}>
-                  Character Level:
-                </label>
-                <input
-                  className="max-w-[55px]"
-                  id={"character-level-input-" + i}
-                  value={threshold.level}
-                  type="number"
-                  onChange={(e) =>
-                    updateThresholdLevel(e.currentTarget.value, i)
-                  }
-                ></input>
-              </div>
-              <div>
-                <label htmlFor={"character-input-" + i}>
-                  Number of Characters at associated level:
-                </label>
-                <input
-                  className="max-w-[55px]"
-                  id={"character-input-" + i}
-                  value={threshold.numChars}
-                  type="number"
-                  onChange={(e) => updateNumChars(e.currentTarget.value, i)}
-                ></input>
-              </div>
-            </div>
-          ))}
+      <div className="border rounded-md border-black mx-auto bg-blue-200">
+        <p className="text-center">
+          Below you will find two buttons that can add or remove rows to this
+          piece of the Encounter Balancer. Each row will have two input fields:
+          one for the level of the characters in your party, and another for how
+          many characters are at that level currently. You can then add more
+          rows for each unique level of characters that comprise the party.
           <br></br>
           <br></br>
-          <div className="text-center text-3xl">
-            <div className="text-green-600">
-              <span>Easy: </span>
-              <span>{calculateXpTotal(xpThresholds, "easy")}</span>
-            </div>
+          For example, we will take a party of five characters, three of which
+          are level 8, and two of which are level 9. There are two different
+          levels represented here so you would need two rows. One row would have
+          the character level field set to 8, and the number of characters field
+          set to 3. The second row would have the character level field set to
+          9, and the number of characters field set to 2. Once these fields have
+          been input, you will see that the difficulty level fields below will
+          auto-populate with the total XP thresholds for the entirety of the
+          party.
+        </p>
+      </div>
+      <br></br>
+      <br></br>
+      <div className="border rounded-3xl border-black mx-auto bg-green-200">
+        <h2 className="text-center font-bold underline">
+          Player Character Rows
+        </h2>
+        <br></br>
+        <div className="text-center">
+          <button
+            className="border rounded-3xl border-black p-2 m-2 hover:bg-white bg-green-400 "
+            onClick={() => {
+              // Add an empty object to the thresholds array
+              setXpThresholds([...xpThresholds, { level: 0, numChars: 0 }]);
+            }}
+          >
+            Add New Character Level
+          </button>
+          <button
+            className="border rounded-3xl border-black p-2 hover:bg-white bg-red-500"
+            onClick={() => {
+              // Remove an object from the thresholds array
+              setXpThresholds([...xpThresholds.slice(0, -1)]);
+            }}
+          >
+            Remove Character Level
+          </button>
+          <br></br>
+          <div>
+            {xpThresholds.map((threshold, i) => (
+              <div key={"threshold" + i} className="flex">
+                <div>
+                  <label htmlFor={"character-level-input-" + i}>
+                    Character Level:
+                  </label>
+                  <input
+                    className="max-w-[55px]"
+                    id={"character-level-input-" + i}
+                    value={threshold.level}
+                    type="number"
+                    onChange={(e) =>
+                      updateThresholdLevel(e.currentTarget.value, i)
+                    }
+                  ></input>
+                </div>
+                <div>
+                  <label htmlFor={"character-input-" + i}>
+                    Number of Characters at associated level:
+                  </label>
+                  <input
+                    className="max-w-[55px]"
+                    id={"character-input-" + i}
+                    value={threshold.numChars}
+                    type="number"
+                    onChange={(e) => updateNumChars(e.currentTarget.value, i)}
+                  ></input>
+                </div>
+              </div>
+            ))}
             <br></br>
             <br></br>
-            <div className="text-yellow-600">
-              <span>Medium: </span>
-              <span>{calculateXpTotal(xpThresholds, "medium")}</span>
-            </div>
-            <br></br>
-            <br></br>
-            <div className="text-red-600">
-              <span>Hard: </span>
-              <span>{calculateXpTotal(xpThresholds, "hard")}</span>
-            </div>
-            <br></br>
-            <br></br>
-            <div className="text-black">
-              <span>Deadly: </span>
-              <span>{calculateXpTotal(xpThresholds, "deadly")}</span>
+            <div className="text-center text-3xl">
+              <div className="text-green-600">
+                <span>Easy: </span>
+                <span>{calculateXpTotal(xpThresholds, "easy")}</span>
+              </div>
+              <br></br>
+              <br></br>
+              <div className="text-yellow-600">
+                <span>Medium: </span>
+                <span>{calculateXpTotal(xpThresholds, "medium")}</span>
+              </div>
+              <br></br>
+              <br></br>
+              <div className="text-red-600">
+                <span>Hard: </span>
+                <span>{calculateXpTotal(xpThresholds, "hard")}</span>
+              </div>
+              <br></br>
+              <br></br>
+              <div className="text-black">
+                <span>Deadly: </span>
+                <span>{calculateXpTotal(xpThresholds, "deadly")}</span>
+              </div>
             </div>
           </div>
         </div>
